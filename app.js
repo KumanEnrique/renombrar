07f0d208ec6dffceec16fa49fbfb64727e9f149a
KumanEnrique <36672948+KumanEnrique@users.noreply.github.com>
2021-07-29 18:31:27 -0500

    listar los archivos de una carpeta

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const fs = require('fs')
 
-fs.readdir("./entrada/",(error,files)=>{
+fs.readdir("./APRENDER A PENSAR-LECTURA/",(error,files)=>{
     if(error){
         console.log(`el error es: ${error}`)
     }else{
-        // console.log(files)
-        const regex = /y2mate.com - /
-        // console.log(regex);
-        files.forEach((file)=>{
-            const match = regex.exec(file)
-            const cadenaNew = file.slice(13,file[file.length])
-            // console.log(cadenaNew);
-            const newPath = __dirname+`/salida/${cadenaNew}`
-            fs.rename('./entrada/'+file,newPath,(error)=>{
-                if(error){
-                    console.log(`el error es: ${error}`)
-                }else{
-                    console.log('exito!!')
-                }
-            })
+        console.log(files)
+        files.forEach(file=>{
+            // fs.writeFileSync('names.txt',file + '\n')
+            fs.appendFileSync('names.txt',file + '\n')
+            console.log('exito!!!');
         })
     }
 })
